2b4747481ef08721d36cb741978a764e6a95664d
xandobler-ai <xandobler@gmail.com>
2026-09-15 18:03:25 +1000

    Update HelloNeighborMultiplayer.cpp

--- HelloNeighborMultiplayer/Source/HelloNeighborMultiplayer /HelloNeighborMultiplayer.cpp
+++ HelloNeighborMultiplayer/Source/HelloNeighborMultiplayer /HelloNeighborMultiplayer.cpp
@@ -1,11 +1,38 @@
 #include "HelloNeighborMultiplayer.h"
+#include "HNPlayerManager.h"
 
 void FHelloNeighborMultiplayerModule::StartupModule()
 {
+    PlayerManager =
+        NewObject<UHNPlayerManager>();
+
+    if (PlayerManager)
+    {
+        PlayerManager->AddToRoot();
+        PlayerManager->Start();
+    }
+
+    UE_LOG(
+        LogTemp,
+        Log,
+        TEXT("[HNMP] Multiplayer plugin started")
+    );
 }
 
 void FHelloNeighborMultiplayerModule::ShutdownModule()
 {
+    if (PlayerManager)
+    {
+        PlayerManager->Stop();
+        PlayerManager->RemoveFromRoot();
+        PlayerManager = nullptr;
+    }
+
+    UE_LOG(
+        LogTemp,
+        Log,
+        TEXT("[HNMP] Multiplayer plugin stopped")
+    );
 }
 
 IMPLEMENT_MODULE(
