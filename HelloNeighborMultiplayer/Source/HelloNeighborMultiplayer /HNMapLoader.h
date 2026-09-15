62b1627324753ec2b332e00e8a3dd96ce1fc39ae
xandobler-ai <xandobler@gmail.com>
2026-09-15 18:33:54 +1000

    Create HNMapLoader.h

---
+++ HelloNeighborMultiplayer/Source/HelloNeighborMultiplayer /HNMapLoader.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "HNMapLoader.generated.h"
+
+UCLASS()
+class HELLONEIGHBORMULTIPLAYER_API UHNMapLoader
+    : public UObject
+{
+    GENERATED_BODY()
+
+public:
+
+    bool LoadMap(UWorld* World, const FString& MapName);
+
+    bool LoadAct1(UWorld* World);
+    bool LoadAct1Day(UWorld* World);
+    bool LoadPlayerHouse(UWorld* World);
+    bool LoadNeighborHouse(UWorld* World);
+    bool LoadAct2(UWorld* World);
+    bool LoadAct3(UWorld* World);
+    bool LoadFinale(UWorld* World);
+
+private:
+
+    bool TravelToMap(
+        UWorld* World,
+        const FString& MapPath
+    );
+};
